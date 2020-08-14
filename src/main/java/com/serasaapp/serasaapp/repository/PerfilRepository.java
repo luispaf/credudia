cfffc0d3651de74e8a70c0a8ebba2ffb53fc8534
Luis Paulo <andradefreitas590@gmail.com>
2020-08-14 13:12:17 -0300

    ok

--- src/main/java/com/serasaapp/serasaapp/repository/PerfilRepository.java
+++
@@ -1,9 +0,0 @@
-package com.serasaapp.serasaapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.serasaapp.serasaapp.domain.Perfil;
-
-public interface PerfilRepository  extends JpaRepository<Perfil, Long> {
-
-}
